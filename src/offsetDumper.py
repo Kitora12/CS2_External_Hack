5543ba2335e21b7aca927e49a97b6aa59d61b791
EtiennePiot <91669483+EtiennePiot@users.noreply.github.com>
2024-02-08 13:48:23 +0100

    Update offsetDumper.py

--- src/offsetDumper.py
+++ src/offsetDumper.py
@@ -7,7 +7,7 @@ class OffsetDumper:
     @staticmethod
     def fetch_and_set_offsets():
         offsets = requests.get("https://raw.githubusercontent.com/a2x/cs2-dumper/main/generated/offsets.json").json()
-        client_dll = requests.get("https://raw.githubusercontent.com/a2x/cs2-dumper/9a13b18e5bddb9bc59d5cd9a3693b39fd8d6849b/generated/client.dll.json").json()
+        client_dll = requests.get("https://raw.githubusercontent.com/a2x/cs2-dumper/main/generated/client.dll.json").json()
 
         for k in offsets["client_dll"]["data"]:
             setattr(Offsets, k, offsets["client_dll"]["data"][k]["value"])
